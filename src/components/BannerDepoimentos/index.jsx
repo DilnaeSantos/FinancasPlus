ed4469a3179ddf87ebbc3befae7cca5d0ec5c9d2
DilnaeSantos <116980369+DilnaeSantos@users.noreply.github.com>
2023-11-07 16:15:13 -0300

    FEAT:  correção da importação da img

--- src/components/BannerDepoimentos/index.jsx
+++ src/components/BannerDepoimentos/index.jsx
@@ -1,9 +1,10 @@
 import { StylesBannerDepoimentos } from './style'
+import lpBanner3 from '../../../public/LP-banner3 1.svg'
 
 const BannerDepoimentos = () => {
     return (
         <StylesBannerDepoimentos>
-            <img className={'img1'} src="../../../public/LP-banner3 1.svg" alt="um homem com nootebok com um grafico"/>
+            <img className={'img1'} src={lpBanner3} alt="um homem com nootebok com um grafico"/>
             <div>
                 <h1>Transformações Financeiras Reais</h1>
                 <p>Descubra como nossos usuários alcançaram suas metas, controlaram suas finanças e conquistaram sonhos que pareciam distantes.</p>
