6f33f54ed0ee0fc954b6f905bc3b85721faaee39
DilnaeSantos <116980369+DilnaeSantos@users.noreply.github.com>
2023-11-07 16:08:12 -0300

    FEAT: correçao na importaçao da img

--- src/components/BannerBemVindo/index.jsx
+++ src/components/BannerBemVindo/index.jsx
@@ -1,5 +1,6 @@
 // import React from 'react'
 import { StylesBannerBemVindo } from '../BannerBemVindo/styles'
+import lpBanner from '../../../public/LP-banner1 1.svg'
 
 const BannerBemVindo = () => {
   return (
@@ -9,7 +10,7 @@ const BannerBemVindo = () => {
         <p className={'paragrafo1'}>O seu aliado para uma gestão financeira bem-sucedida e a realização dos seus objetivos financeiros.</p>
         <p className={'paragrafo2'}>Nosso aplicativo foi projetado para proporcionar a você o controle total sobre suas finanças pessoais, tornando o processo de gerenciar suas receitas, despesas e metas financeiras uma tarefa simples e prazerosa.</p>
       </div>
-        <img className={'img1'} src="../../../public/LP-banner1 1.svg" alt="mulher em cima de moedas"/> 
+        <img className={'img1'} src={lpBanner} alt="mulher em cima de moedas"/> 
      </StylesBannerBemVindo>  
   )
 };
